8f5527fd31c3f31e3bdbd97cb8232809a324e016
Nikhil N S <nikhilshivaraj06@gmail.com>
2026-08-04 14:13:44 +0530

    Improve HTML views

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_07_30_143539) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "expense_splits", force: :cascade do |t|
     t.decimal "amount"
     t.datetime "created_at", null: false
